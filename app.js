5e34535a75b38fe41deb1f1a439e6c89a000f0f9
freyder montano <freymanuser@gmail.com>
2023-01-31 00:01:44 -0300

    creando id de cada producto y pode obtenerlos

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let total = 0;
 
 productoContainer = document.querySelector('.shop-items');
 //elegir boton anadir al cart
+//seleccionar car items para agrehgar producto seleccioado
+let cardContainer = document.querySelector('.cart-items');
 
 
 
@@ -22,7 +24,7 @@ const fechtProducto = async () => {
     productoArray.forEach(product => {
         productoContainer.innerHTML += `
         <div class="col-sm-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center align-items-center">
-            <div class="card shadow" style="width: 18rem;">
+            <div class="card shadow" style="width: 18rem;" id="${product.id}">
                 <img src="${product.images[0]} " class="card-img-top" alt="producto">
                 <div class="card-body p-4">
                     <h5 class="card-title">${product.title}</h5>
@@ -37,7 +39,56 @@ const fechtProducto = async () => {
     });
 
     let addBtnProduct = document.querySelectorAll('.shop-item-button');
+
+    //para comvertirlo a un array transformar nodelist a array
+    addBtnProduct = [...addBtnProduct];
     console.log(addBtnProduct);
+
+    //funcion cuando se hace click a un boton add
+    addBtnProduct.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            //console.log('click');
+            //agrgar productos al carro
+
+
+
+            //buscar el id del producto
+            console.log(e);
+
+
+
+            //con el id obtner el objecto actual
+
+
+            //agregar el producto al carro
+
+
+
+            cardContainer.innerHTML += `
+           
+            <div class="cart-items row d-flex align-items-center">
+                <div class="col-4 d-flex align-items-center">
+                    <img src="./images/img2.jpg" alt="producto" class="img-fluid w-25">
+                    <span class="card-item-title">shirt</span>
+                </div> 
+
+                <div class="col-4">
+                    <span class="car-price cart-column">$ 199990</span>
+                </div>
+
+                <div class="col-4">
+                    <input type="number" class="card-cuantity-input" min="1">
+                    <button class="btn btn-danger" type="button">REMOVE</button>
+                </div>
+            </div>
+      
+            
+            `;
+
+        })
+    });
+
+
 
 }
 
