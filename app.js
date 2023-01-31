b8c35bcf86da43c7a7bd5ecc4b8259924b0cb3b0
freyder montano <freymanuser@gmail.com>
2023-01-30 22:55:46 -0300

    retructurando la pagina

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 
+//variables iniciales
 let shoppingCartArray = [];
 let total = 0;
 
-productoContainer = document.querySelector('.shop-items')
+productoContainer = document.querySelector('.shop-items');
+//elegir boton anadir al cart
+
 
 
 
 const fechtProducto = async () => {
 
-    const response = await fetch('https://api.escuelajs.co/api/v1/products');
+    const response = await fetch('https://api.escuelajs.co/api/v1/products/');
     const data = await response.json();
 
     console.log(data);
-    let productoArray = data;
+    let productoArray = data.slice(1, 25);
 
 
+    //imprimir productos en pantalla
     productoArray.forEach(product => {
-
         productoContainer.innerHTML += `
-        <div class="col-md-4 ">
-                <div class="card" style="width: 18rem;">
-                    <img src="${product.images[0]} " class="card-img-top" alt="producto">
-                    <div class="card-body p-4">
-                        <h5 class="card-title">${product.title}</h5>
-                        <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                            <span class="shop-item-price">${product.price}</span>
-                        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center align-items-center">
+            <div class="card shadow" style="width: 18rem;">
+                <img src="${product.images[0]} " class="card-img-top" alt="producto">
+                <div class="card-body p-4">
+                    <h5 class="card-title">${product.title}</h5>
+                    <div class="d-flex justify-content-between">
+                        <a href="#" class="btn btn-primary shop-item-button">ADD TO CARD</a>
+                        <span class="shop-item-price">$ ${product.price}</span>
                     </div>
                 </div>
+            </div>
+        </div>
         `;
-
     });
+
+    let addBtnProduct = document.querySelectorAll('.shop-item-button');
+    console.log(addBtnProduct);
 
 }
 
-
 fechtProducto();
+
